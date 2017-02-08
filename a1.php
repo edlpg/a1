00610751228b4664ad32fe6b53e61d738116f9bd
Eduardo de la Pena <edelapena@g.harvard.edu>
2017-02-07 23:26:04 -0600

    Modified title in README file.

---
+++ a1.php
@@ -0,0 +1,12 @@
+<h1 style="color:blue;">Eduardo de la Pena</h1>
+<img src='me.jpg' alt='Me'>
+<p>I have a Bacherlor's in animation and Digital Art and I'm</p>
+<p>taking this course because since I already know html, css</p>
+<p>js I want to know the comlete web knowledge, so only server</p>
+<p>Side was missing.</p>
+<p>That's what I expect to learn on this course.</p>
+<br><br>
+<h2 style="color:blue;">Random Quote</h2>
+<h3 style="color:blue;">I like foxes</h3>
+
+<img src='fox.jpg' alt='Adorable fox'>
